3b6e0cff2039feeb21608a12f26b54469dd140ee
AbdullahiAA <osuolaleabdullahi@gmail.com>
2023-02-20 10:27:09 +0100

    Update

--- Styler02/App.tsx
+++ Styler02/App.tsx
@@ -1,12 +1,10 @@
 import {Text, View} from 'react-native';
 import React from 'react';
-import FlatCards from './components/FlatCards';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
-      <FlatCards />
     </View>
   );
 };
